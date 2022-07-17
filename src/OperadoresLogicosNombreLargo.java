9963e65fbf216c55c423f0d234895cb54c17af21
Juan Camilo Cartagena <camilocartagena@gmail.com>
2022-07-16 19:59:10 -0500

    ejercicio operadores logicos 3 ajustes

--- src/OperadoresLogicosNombreLargo.java
+++ src/OperadoresLogicosNombreLargo.java
@@ -3,17 +3,18 @@ import javax.swing.*;
 public class OperadoresLogicosNombreLargo {
     public static void main(String[] args) {
         String nombreCompleto1 = JOptionPane.showInputDialog("Ingrese el Nombre y Apellido de la persona 1");
-        String[] nombre1 = nombreCompleto1.split(" ");
         String nombreCompleto2 = JOptionPane.showInputDialog("Ingrese el Nombre y Apellido de la persona 2");
-        String[] nombre2 = nombreCompleto2.split(" ");
         String nombreCompleto3 = JOptionPane.showInputDialog("Ingrese el Nombre y Apellido de la persona 3");
-        String[] nombre3 = nombreCompleto3.split(" ");
 
-        if(nombre1[0].length() > nombre2[0].length() && nombre1[0].length() > nombre3[0].length()){
+        String resultado = nombreCompleto1.split(" ")[0].length() > nombreCompleto2.split(" ")[0].length() ?  nombreCompleto1 : nombreCompleto2;
+        resultado = resultado.split(" ")[0].length() > nombreCompleto3.split(" ")[0].length() ?  resultado : nombreCompleto3;
+
+
+        if(nombreCompleto1.split(" ")[0].length() > nombreCompleto2.split(" ")[0].length() && nombreCompleto1.split(" ")[0].length() > nombreCompleto3.split(" ")[0].length()){
             JOptionPane.showMessageDialog(null,nombreCompleto1 + " tiene el nombre mas largo");
-        }else if (nombre2[0].length() > nombre1[0].length() && nombre2[0].length() > nombre3[0].length()){
+        }else if(nombreCompleto2.split(" ")[0].length() > nombreCompleto1.split(" ")[0].length() && nombreCompleto2.split(" ")[0].length() > nombreCompleto3.split(" ")[0].length()){
             JOptionPane.showMessageDialog(null,nombreCompleto2 + " tiene el nombre mas largo");
-        }else if (nombre3[0].length() > nombre1[0].length() && nombre3[0].length() > nombre2[0].length()){
+        }else if(nombreCompleto3.split(" ")[0].length() > nombreCompleto1.split(" ")[0].length() && nombreCompleto3.split(" ")[0].length() > nombreCompleto2.split(" ")[0].length()){
             JOptionPane.showMessageDialog(null,nombreCompleto3 + " tiene el nombre mas largo");
         }else{
             JOptionPane.showMessageDialog(null,"Existe empate y debes volver a ingresarlos");
