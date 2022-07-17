9c73b336a156f5cd71a86ba22dc3b5d23d6b65df
Juan Camilo Cartagena <camilocartagena@gmail.com>
2022-07-16 19:28:05 -0500

    ejercicio operadores logicos 2 ajuste

--- src/OperadoresLogicosGasolina.java
+++ src/OperadoresLogicosGasolina.java
@@ -9,21 +9,21 @@ public class OperadoresLogicosGasolina {
 
         if(medidaActual >= 1 && medidaActual <=20){
             resultado = "Insuficiente";
-        }
-        if(medidaActual >= 20 && medidaActual <=35){
+        }else if(medidaActual >= 20 && medidaActual <=35){
             resultado = "Suficiente";
-        }
-        if(medidaActual >= 35 && medidaActual <=40){
+        }else if(medidaActual >= 35 && medidaActual <=40){
             resultado = "Medio Tanque";
-        }
-        if(medidaActual >= 40 && medidaActual <=60){
+        }else if(medidaActual >= 40 && medidaActual <=60){
             resultado = "Tanque 3/4";
-        }
-        if(medidaActual >= 60 && medidaActual <=70){
+        }else if(medidaActual >= 60 && medidaActual <=70){
             resultado = "Tanque casi lleno";
-        }
-        if(medidaActual >= 70 ){
+        }else if(medidaActual == 70 ){
             resultado = "Tanque  lleno";
+        }else{
+            resultado = "Debe ingresar un valor entre 1 y 70";
+            System.out.println(resultado);
+            main(args);
+            System.exit(0);
         }
         System.out.println(resultado);
     }
